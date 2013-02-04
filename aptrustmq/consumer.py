038b0f7eef40c0853a931fc7db9beed03c304115
Streamweaver <Streamweaver@gmail.com>
2013-02-04 17:02:37 -0500

    Added better logging.

--- aptrustmq/consumer.py
+++ aptrustmq/consumer.py
@@ -9,6 +9,8 @@ Created on Feb 1, 2013
 @author: swt8w
 '''
 from datetime import datetime
+import logging
+
 from kombu.mixins import ConsumerMixin
 from kombu import Connection, Queue, Exchange
 
@@ -20,22 +22,40 @@ except ImportError:
     mqconfig.py for this app.  See mqconfig_dist.py for details'''
     del sys
 
-class HeartbeatConsumer(ConsumerMixin):
+
+
+class LoggingConsumer(ConsumerMixin):
     
     def __init__(self, conn, exchng, rt_key, ack=False):
+        """Sets up a basic consumer that logs incomming messages.  Use this
+        to listen for heartbeat and other test messages.
+
+        :param conn:  Connection object to amqp server.
+        :param exchng:  String of exchange to use on conn.
+        :param rt_key:  String of routing key to use for message.
+        :param logfile:  Sting of filename to use for logger.
+        :param ack:  Boolean of acknowlege flag for consumer.
+
+        """
         self.connection = conn
         self.default_queue = Queue(QUEUE, exchange=Exchange(exchng), routing_key=rt_key)
         self.ack = ack
+        # Setup appropriate logger
+        logfilename = "%s_%s_%s.log" % (exchng, QUEUE, rt_key)
+        logging.basicConfig(filename=logfilename,format='%(asctime)s %(message)s', 
+            datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
         
     def get_consumers(self, Consumer, chan):
         return [Consumer(queues=[self.default_queue,], callbacks=[self.on_message,], auto_declare=False)]
         
     def on_message(self, body, msg):
-        print("%s %s" % (datetime.today(), body))
+        logging.info("%r" % body)
+        print("%s %r" % (datetime.today(), body))
         if self.ack:
             msg.ack()
             
 if __name__ == '__main__':
     conn = Connection(AMQPURL)
-    cnsmr = HeartbeatConsumer(conn, EXCHANGE, ROUTING_KEY, ack=True)
+    cnsmr = LoggingConsumer(conn, EXCHANGE, ROUTING_KEY, ack=True)
+    print("Logging messages from %s->%s->%s.  Press CTRL+C to exit." % (EXCHANGE, QUEUE, ROUTING_KEY))
     cnsmr.run()
