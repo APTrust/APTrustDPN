a872ca9c6da5a65ebf5ee7fddd49b0bed9a8890a
Streamweaver <Streamweaver@gmail.com>
2013-04-08 20:53:36 -0400

    Shaping up messages again.

--- dpnode/dpnmq/messages.py
+++ dpnode/dpnmq/messages.py
@@ -4,7 +4,7 @@ import logging
 from kombu import Connection
 
 from dpnode.settings import DPNMQ
-from dpnmq.util import dpn_strftime
+from dpnmq.util import dpn_strftime, is_string
 
 logger = logging.getLogger('dpnmq.console')
 
@@ -20,14 +20,15 @@ class DPNMessage(object):
         Base Message object for DPN.
         """
         self.set_headers()
+        self.body = {}
 
     def set_headers(self, frm=DPNMQ["NODE"], 
-        reply_key=DPNMQ["LOCAL"]["ROUTINGKEY"], ttl=DPNMQ["TTL"], id=None, 
-        sequence=None, date=None):
+        reply_key=DPNMQ["LOCAL"]["ROUTINGKEY"], ttl=DPNMQ["TTL"], 
+        correlation_id=None, sequence=None, date=None, **kwargs):
         self.headers = { 
-            'from': self.src_node,
-            'reply_key': self.routing_key,
-            'correlation_id': "%s" % self.id,
+            'from': frm,
+            'reply_key': reply_key,
+            'correlation_id': "%s" % correlation_id,
             'sequence': self.sequence,
             'date': self.date,
             'ttl': self.ttl,
@@ -43,6 +44,16 @@ class DPNMessage(object):
         for key, value in self.headers.iteritems():
             if value is None:
                 raise DPNMessageError("No header value set for %s." % (key,))
+        for key in ['from', 'reply_key', 'correlation_id', 'date']:
+            if not is_string(self.headers[key]):
+                raise DPNMessageError(
+                            "Header value of %s for '%s' is not a string!" % 
+                            (self.headers[key], key))
+        for key in ['sequence', 'ttl']:
+            if not isinstance(self.headers[key], int):
+                raise DPNMessageError(
+                            "Header value of %s for '%s' is not an int!" % 
+                            (self.headers[key], key))
 
 
     def send(self, rt_key):
@@ -83,14 +94,50 @@ class DPNMessage(object):
         raise NotImplementedError("Must implement a body validation method.")
 
 
-class QueryForReplication(DPNMessage):
+class ReplicationInitQuery(DPNMessage):
+    directive = 'replication-init-query'
+    def __init__(self, replication_size):
+        super(ReplicationInitQuery, self):__init__()
+        self.set_headers(**{
+            'id': ,
+            'sequence': 0,
+            'date':  dpn_strftime(datetime.now()),
+            })
+        self.body = {
+            'message_name': self.directive,
+            'replication_size': replication_size,
+            'protocol': DPNMQ["XFER_OPTIONS"]
+        }
+
+    def validate_body(self):
+        classname = self.__class__.__name__
+        if not self.body["message_name"] == self.directive:
+            raise DPNMessage("%s.body['message_name] is not %s!" % 
+                                                    (classname, self.directive))
+        if not isinstance(self.body['replication_size'], int):
+            raise DPNMessage("%s.body['replication_size'] of %s is not an int!"
+                                 % (classname, self.body["replication_size"]))
+        for prtcl in self.body["protocol"]:
+            protocols = ['https', 'rsync']
+            if prtcl not in protocols:
+                raise DPNMessage(
+                            "%s.body['protocol'] value %s is not one of %s!" % 
+                                            (classname, self.body["protocol"]))
+
+
+class ReplicationAvailableReply(DPNMessage):
     pass
 
-class ContentLocation(DPNMessage):
+class ReplicationLocationReply(DPNMessage):
     pass
 
-class TransferStatus(DPNMessage):
+class ReplicationLocationCancel(DPNMessage):
     pass
 
-class RegistryItemCreation(DPNMessage):
+class ReplicationTransferReply(DPNMessage):
     pass
+
+class ReplicationVerificationReply(DPNMessage):
+    pass
+
+
