1f91cc865fb5eba72f747130de9a79de80f5d0b3
Streamweaver <Streamweaver@gmail.com>
2013-05-02 00:19:53 -0400

    Setup control flow tracking app.

---
+++ dpnode/dpn_control_flow/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class CopyAction(models.Model):
+    correlation_id = models.CharField(max_length=100)
+    node = models.CharField(max_length=5)
+    protocol = models.CharField(max_length=5)
+    location = models.TextField()
+
+    # Workflow Tracking
+    step = models.TextField()
+    state = models.CharField(max_length=10)
+    note = models.TextField(blank=True, null=True)
+
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (('correlation_id', 'node'))
+        ordering = ['-updated_at']
