1be499e2b32c0f022b1bc062a7ba56eafe000fb8
Helmy Giacoman <helmygb@gmail.com>
2014-03-31 12:06:52 -0600

    Watcher now waits to get the final size of the bag

--- dpnode/dpnmq/management/commands/dpn_bag_watcher.py
+++ dpnode/dpnmq/management/commands/dpn_bag_watcher.py
@@ -37,13 +37,31 @@ class Command(BaseCommand):
 class DPNFileEventHandler(PatternMatchingEventHandler):
 
     def on_created(self, event):
-        if not event.is_directory:            
+        if not event.is_directory:
             base = os.path.basename(event.src_path)            
-            filesize = os.path.getsize(event.src_path)
+            initial_filesize = os.path.getsize(event.src_path)            
             filename = os.path.splitext(base)[0] # filename to be used as id
 
+            logger.info("New bag detected: %s. Let's wait 5 seconds and check size again..." % base)
+            while True:
+                # wait 5 seconds to check bag size again
+                time.sleep(5)
+                filesize_now = os.path.getsize(event.src_path)
+                
+                # if initial filesize is equal to the filesize now
+                # we can start the bag ingestion
+                if initial_filesize == filesize_now:
+                    filesize = filesize_now
+                    break
+                else:
+                    initial_filesize = filesize_now
+                    print("Bag is not ready, check again in 5 seconds...")
+
+                # NOTE: how long should we wait to get the final size of the bag??
+                # discuss this with the team
+            
             if filesize < DPN_MAX_SIZE:
                 initiate_ingest(filename, filesize)
-                logger.info("New bag detected: %s. Starting ingestion..." % base)
+                logger.info("Bag size looks good... starting ingestion of %s." % base)
             else:
                 logger.info("Bag %s is too big to be replicated. Not ingested!" % base)
