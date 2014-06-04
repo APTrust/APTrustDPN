8e3829adc405d53f517355035f078ea9236ae0bc
Scott Turnbull <streamweaver@gmail.com>
2014-06-04 18:15:39 +0000

    Added list display and filters to models view.

--- dpnode/dpn_registry/admin.py
+++ dpnode/dpn_registry/admin.py
@@ -6,9 +6,11 @@ class NodeAdmin(admin.ModelAdmin):
 admin.site.register(Node, NodeAdmin)
 
 class RegistryEntryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('dpn_object_id', 'first_node_name', 'bag_size', 'last_modified_date')
+    list_filter = ('first_node_name',)
 admin.site.register(RegistryEntry, RegistryEntryAdmin)
 
 class NodeEntryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('dpn_object_id', 'first_node_name', 'node', 'bag_size', 'last_modified_date')
+    list_filter = ('first_node_name', 'node')
 admin.site.register(NodeEntry, NodeEntryAdmin)
--- dpnode/dpn_workflows/admin.py
+++ dpnode/dpn_workflows/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from dpn_workflows.models import SendFileAction, ReceiveFileAction, NodeInfo
 
 class SendFileActionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('ingest', 'node', 'step', 'state', 'chosen_to_transfer')
+    list_filter = ('step', 'state', 'node', 'chosen_to_transfer')
 admin.site.register(SendFileAction, SendFileActionAdmin)
 
 class ReceiveFileActionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('correlation_id', 'node', 'step', 'state',)
+    list_filter = ('step', 'state', 'node')
 admin.site.register(ReceiveFileAction, ReceiveFileActionAdmin)
 
 class NodeInfoAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
+    list_display = ('name', 'slug')
 admin.site.register(NodeInfo, NodeInfoAdmin)
