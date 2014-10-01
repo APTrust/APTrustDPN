84946195ad35d207c56d0936cf7ca2dadd2ce4d9
Streamweaver <Streamweaver@gmail.com>
2014-10-01 13:10:01 -0400

    Adding per node storage stat view.

--- dpnode/dpn_registry/views.py
+++ dpnode/dpn_registry/views.py
@@ -2,22 +2,31 @@ from django.db.models import Sum, Max, Avg, Count
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-from django.conf import settings
 
-from dpn_registry.models import RegistryEntry
+from dpn_registry.models import RegistryEntry, Node
+
 
 @login_required
 def index(request):
     entries = RegistryEntry.objects.all()
-    totals = entries.aggregate(Sum('bag_size'), Max('bag_size'), Avg('bag_size'))
+    totals = entries.aggregate(Sum('bag_size'), Max('bag_size'),
+                               Avg('bag_size'))
 
-    node_totals = entries.values('first_node_name').annotate(Sum('bag_size'), Max('bag_size'), Avg('bag_size'), Count("bag_size"))
-    local_totals = entries.filter(replicating_nodes__name=settings.DPN_NODE_NAME).aggregate(Count('bag_size'), Sum('bag_size'))
+    node_totals = entries.values('first_node_name').annotate(Sum('bag_size'),
+                                                             Max('bag_size'),
+                                                             Avg('bag_size'),
+                                                             Count("bag_size"))
+
+    local_totals = Node.objects.annotate(
+        bag_count=Count('registryentry__bag_size'),
+        total_size=Sum('registryentry__bag_size'))
+
+
     return render_to_response("index.html", {
         'count': entries.count(),
         'totals': totals,
         'local_totals': local_totals,
         'node_totals': node_totals,
-        },
-        context_instance=RequestContext(request)
+    },
+                              context_instance=RequestContext(request)
     )
